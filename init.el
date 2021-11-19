3a2c8eb877e08e37d73fdba34c1dea2f6aa81a05
Romulo <romulosnoliveira@gmail.com>
2021-11-19 10:21:15 -0300

    Alteração de tema, adiciona icones, doomline e outros ajustes

--- init.el
+++ init.el
@@ -13,30 +13,13 @@
 	 ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(ansi-term-color-vector
 	 [unspecified "#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"] t)
- '(custom-enabled-themes '(spacemacs-dark))
  '(custom-safe-themes
-	 '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "e29a6c66d4c383dbda21f48effe83a1c2a1058a17ac506d60889aba36685ed94" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "36ca8f60565af20ef4f30783aa16a26d96c02df7b4e54e9900a5138fb33808da" "bf798e9e8ff00d4bf2512597f36e5a135ce48e477ce88a0764cfb5d8104e8163" "76b4632612953d1a8976d983c4fdf5c3af92d216e2f87ce2b0726a1f37606158" "d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" "a325ba05dc3b5c2fa89af0ff354bbbe90251fb1a6e6d5682977cebe61ce72ab7" "3b8284e207ff93dfc5e5ada8b7b00a3305351a3fb222782d8033a400a48eca48" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default))
- '(fci-rule-character-color "#d9d9d9")
- '(fci-rule-color "#d9d9d9")
- '(hl-todo-keyword-faces
-	 '(("TODO" . "#dc752f")
-		 ("NEXT" . "#dc752f")
-		 ("THEM" . "#2d9574")
-		 ("PROG" . "#4f97d7")
-		 ("OKAY" . "#4f97d7")
-		 ("DONT" . "#f2241f")
-		 ("FAIL" . "#f2241f")
-		 ("DONE" . "#86dc2f")
-		 ("NOTE" . "#b1951d")
-		 ("KLUDGE" . "#b1951d")
-		 ("HACK" . "#b1951d")
-		 ("TEMP" . "#b1951d")
-		 ("FIXME" . "#dc752f")
-		 ("XXX+" . "#dc752f")
-		 ("\\?\\?\\?+" . "#dc752f")))
+	 (quote
+		("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "36f17556e827b41b63fe9375dbeeb4989d4976fe51cd0506968c6a41d2a7c9f8")))
  '(package-selected-packages
-	 '(org-brain smart-mode-line-atom-one-dark-theme smart-mode-line: atom-one-dark smart-mode-line atom-one-dark-theme zenburn-theme org-mind-map helm-rg evil-mode lsp-ui-peek-mode lsp-ui-peek magit maggit helm-projectile helm projectile ## spacemacs-dark org-mode spacemacs-theme npm-mode lsp-dart emmet-mode which-key js2-mode js-mode typescript-mode yasnippet dap-mode lsp-treemacs lsp-ui flycheck company lsp-mode use-package))
- '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e"))
+	 (quote
+		(doom-modeline command-log-mode dired rainbow-delimiters dired-single all-the-icons-dired all-the-icons org-brain smart-mode-line-atom-one-dark-theme smart-mode-line: atom-one-dark smart-mode-line atom-one-dark-theme zenburn-theme org-mind-map helm-rg evil-mode lsp-ui-peek-mode lsp-ui-peek magit maggit helm-projectile helm projectile ## spacemacs-dark org-mode spacemacs-theme npm-mode lsp-dart emmet-mode which-key js2-mode js-mode typescript-mode yasnippet dap-mode lsp-treemacs lsp-ui flycheck company lsp-mode use-package)))
+ '(pdf-view-midnight-colors (quote ("#b2b2b2" . "#292b2e")))
  '(tetris-x-colors
 	 [[229 192 123]
 		[97 175 239]
@@ -51,6 +34,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(line-number ((t (:inherit default)))))
+
+(load-theme 'wombat)
 
 (global-linum-mode t)
 (global-visual-line-mode t)
@@ -136,7 +121,7 @@
 
 (use-package lsp-treemacs
 	:config
-		(local-set-key (kbd "C-l T v") 'lsp-treemacs-symbols)
+	(local-set-key (kbd "C-l T v") 'lsp-treemacs-symbols)
   :commands
 	lsp-treemacs-errors-list
 	:init
@@ -144,9 +129,11 @@
   :ensure t)
 
 (use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
   :config
-  (which-key-mode)
-  :ensure t)
+  (setq which-key-idle-delay 0.3)
+	:ensure t)
 
 (use-package term
   :config
@@ -170,16 +157,13 @@
 (put 'dired-find-alternate-file 'disabled nil)
 
 (use-package emmet-mode
+	:config
+	(setq emmet-move-cursor-between-quotes t)
   :ensure t)
 
 (use-package mhtml-mode
 	:hook
 	(mhtml-mode . emmet-mode))
-
-(use-package spacemacs-theme
-  :defer t
-  :init (load-theme 'spacemacs-dark t)
-	:ensure t)
 
 (use-package org
 	:ensure t)
@@ -221,7 +205,43 @@
 	(setq sml/theme 'respectful))
 (smart-mode-line-enable)
 
+(when (display-graphic-p)
+  (require 'all-the-icons))
+;; or
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+(use-package all-the-icons-dired
+  :hook (dired-mode . all-the-icons-dired-mode)
+	:ensure t)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode)
+	:ensure t)
+
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+	:bind (("C-x C-j" . dired-jump))
+  :custom ((dired-listing-switches "-agho --group-directories-first")))
+
+(use-package dired-single
+	:ensure t)
+
+(use-package command-log-mode
+	:ensure t)
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
 ;; Functions
 (defun npm-start()
 	(interactive)
 	(npm-mode--exec-process "npm start"))
+
+
+
+
+
