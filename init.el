bbf8f047ef954a6506feb2dc8cd110e46860e83d
Romulo <romulosnoliveira@gmail.com>
2021-11-19 10:45:03 -0300

    Corrige erros e volta o tema spacemacs

--- init.el
+++ init.el
@@ -35,8 +35,6 @@
  ;; If there is more than one, they won't work right.
  '(line-number ((t (:inherit default)))))
 
-(load-theme 'wombat)
-
 (global-linum-mode t)
 (global-visual-line-mode t)
 (show-paren-mode t)
@@ -69,6 +67,11 @@
 	(define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
 	(define-key global-map [remap execute-extended-command] 'helm-M-x)
 	(define-key global-map [remap apropos-command] 'helm-apropos)
+	:ensure t)
+
+(use-package spacemacs-theme
+  :defer t
+  :init (load-theme 'spacemacs-dark t)
 	:ensure t)
 
 (use-package ido
@@ -165,8 +168,52 @@
 	:hook
 	(mhtml-mode . emmet-mode))
 
+(defun dw/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (auto-fill-mode 0)
+  (visual-line-mode 1)
+  (setq evil-auto-indent nil))
+
 (use-package org
+	:hook (org-mode . dw/org-mode-setup)
+  :config (setq org-ellipsis " ▾"
+								org-hide-emphasis-markers t)
 	:ensure t)
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●"))
+	:ensure t)
+
+;; Replace list hyphen with dot
+(font-lock-add-keywords 'org-mode
+                        '(("^ *\\([-]\\) "
+													 (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(with-eval-after-load 'org-faces
+	(dolist (face '((org-level-1 . 1.2)
+									(org-level-2 . 1.1)
+									(org-level-3 . 1.05)
+									(org-level-4 . 1.0)
+									(org-level-5 . 1.1)
+									(org-level-6 . 1.1)
+									(org-level-7 . 1.1)
+									(org-level-8 . 1.1)))
+		(set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+;; Make sure org-indent face is available
+(require 'org-indent)
+
+;; Ensure that anything that should be fixed-pitch in Org files appears that way
+(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+(set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
 
 (use-package npm-mode
 	:ensure t)
@@ -195,10 +242,6 @@
 (use-package vimrc-mode
 	:ensure t)
 
-(use-package atom-one-dark-theme
-	:ensure t)
-(load-theme 'atom-one-dark t)
-
 (use-package smart-mode-line
 	:ensure t
 	:config
@@ -226,6 +269,7 @@
   :custom ((dired-listing-switches "-agho --group-directories-first")))
 
 (use-package dired-single
+	:commands (dired dired-jump)
 	:ensure t)
 
 (use-package command-log-mode
